eb5e067fae1ae21d49e97985afa12f5cf6196fbf
124Dracula <50019422+124Dracula@users.noreply.github.com>
2022-11-14 17:49:43 +0000

    commit

--- criacaoBaseDadosPrimeG.sql
+++ criacaoBaseDadosPrimeG.sql
@@ -39,6 +39,43 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `primeG`.`Localidade`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `primeG`.`Localidade` (
+  `idLocalidade` INT NOT NULL,
+  `Nome` VARCHAR(45) NULL,
+  PRIMARY KEY (`idLocalidade`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `primeG`.`Espaco`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `primeG`.`Espaco` (
+  `idEspaco` INT NOT NULL,
+  `nome` VARCHAR(45) NULL,
+  `rua` VARCHAR(45) NULL,
+  `contacto` VARCHAR(45) NULL,
+  `Utilizador_idUtilizador` INT NOT NULL,
+  `Localidade_idLocalidade` INT NOT NULL,
+  `numeroMesas` INT NOT NULL,
+  PRIMARY KEY (`idEspaco`),
+  INDEX `fk_Espaco_Utilizador1_idx` (`Utilizador_idUtilizador` ASC) VISIBLE,
+  INDEX `fk_Espaco_Localidade1_idx` (`Localidade_idLocalidade` ASC) VISIBLE,
+  CONSTRAINT `fk_Espaco_Utilizador1`
+    FOREIGN KEY (`Utilizador_idUtilizador`)
+    REFERENCES `primeG`.`Utilizador` (`idUtilizador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Espaco_Localidade1`
+    FOREIGN KEY (`Localidade_idLocalidade`)
+    REFERENCES `primeG`.`Localidade` (`idLocalidade`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `primeG`.`Torneio`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `primeG`.`Torneio` (
@@ -47,16 +84,16 @@ CREATE TABLE IF NOT EXISTS `primeG`.`Torneio` (
   `idOrganizador` INT NOT NULL,
   `idDesporto` INT NOT NULL,
   `isFederado` BINARY(1) NOT NULL,
-  `localTorneio` VARCHAR(45) NOT NULL,
-  `numeroMesas` INT NOT NULL,
   `dataTorneio` DATETIME NOT NULL,
   `inscricoesAbertas` BINARY(1) NOT NULL,
   `escalao` INT NOT NULL,
   `tipoTorneio` VARCHAR(7) NOT NULL,
   `terminado` BINARY(1) NOT NULL,
+  `Espaco_idEspaco` INT NOT NULL,
   PRIMARY KEY (`idTorneio`),
   INDEX `fk_Torneio_Utilizador_idx` (`idOrganizador` ASC) VISIBLE,
   INDEX `fk_Torneio_Desporto1_idx` (`idDesporto` ASC) VISIBLE,
+  INDEX `fk_Torneio_Espaco1_idx` (`Espaco_idEspaco` ASC) VISIBLE,
   CONSTRAINT `fk_Torneio_Utilizador`
     FOREIGN KEY (`idOrganizador`)
     REFERENCES `primeG`.`Utilizador` (`idUtilizador`)
@@ -66,18 +103,24 @@ CREATE TABLE IF NOT EXISTS `primeG`.`Torneio` (
     FOREIGN KEY (`idDesporto`)
     REFERENCES `primeG`.`Desporto` (`idDesporto`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Torneio_Espaco1`
+    FOREIGN KEY (`Espaco_idEspaco`)
+    REFERENCES `primeG`.`Espaco` (`idEspaco`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `primeG`.`Utilizador_has_Desporto`
+-- Table `primeG`.`DesportosFav`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `primeG`.`Utilizador_has_Desporto` (
+CREATE TABLE IF NOT EXISTS `primeG`.`DesportosFav` (
   `Utilizador_idUtilizador` INT NOT NULL,
   `Desporto_idDesporto` INT NOT NULL,
-  `numeroFederado` VARCHAR(45) NOT NULL,
-  `clube` VARCHAR(45) NOT NULL,
+  `numeroFederado` VARCHAR(45) NULL,
+  `clube` VARCHAR(45) NULL,
+  `notificacao` INT NULL,
   PRIMARY KEY (`Utilizador_idUtilizador`, `Desporto_idDesporto`),
   INDEX `fk_Utilizador_has_Desporto_Desporto1_idx` (`Desporto_idDesporto` ASC) VISIBLE,
   INDEX `fk_Utilizador_has_Desporto_Utilizador1_idx` (`Utilizador_idUtilizador` ASC) VISIBLE,
@@ -198,6 +241,7 @@ CREATE TABLE IF NOT EXISTS `primeG`.`Jogo` (
   `resultado` VARCHAR(90) NULL,
   `Grupo_idGrupo` INT NULL,
   `Ronda_idRonda` INT NULL,
+  `estado` INT NOT NULL,
   PRIMARY KEY (`idJogo`),
   INDEX `fk_Jogo_Grupo1_idx` (`Grupo_idGrupo` ASC) VISIBLE,
   INDEX `fk_Jogo_Ronda1_idx` (`Ronda_idRonda` ASC) VISIBLE,
@@ -267,6 +311,50 @@ CREATE TABLE IF NOT EXISTS `primeG`.`Torneio_has_Equipa` (
   CONSTRAINT `fk_Torneio_has_Equipa_Equipa1`
     FOREIGN KEY (`Equipa_idEquipa`)
     REFERENCES `primeG`.`Equipa` (`idEquipa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `primeG`.`localFav`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `primeG`.`localFav` (
+  `Localidade_idLocalidade` INT NOT NULL,
+  `Utilizador_idUtilizador` INT NOT NULL,
+  PRIMARY KEY (`Localidade_idLocalidade`, `Utilizador_idUtilizador`),
+  INDEX `fk_Localidade_has_Utilizador_Utilizador1_idx` (`Utilizador_idUtilizador` ASC) VISIBLE,
+  INDEX `fk_Localidade_has_Utilizador_Localidade1_idx` (`Localidade_idLocalidade` ASC) VISIBLE,
+  CONSTRAINT `fk_Localidade_has_Utilizador_Localidade1`
+    FOREIGN KEY (`Localidade_idLocalidade`)
+    REFERENCES `primeG`.`Localidade` (`idLocalidade`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Localidade_has_Utilizador_Utilizador1`
+    FOREIGN KEY (`Utilizador_idUtilizador`)
+    REFERENCES `primeG`.`Utilizador` (`idUtilizador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `primeG`.`TorneiosFav`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `primeG`.`TorneiosFav` (
+  `Utilizador_idUtilizador` INT NOT NULL,
+  `Torneio_idTorneio` INT NOT NULL,
+  PRIMARY KEY (`Utilizador_idUtilizador`, `Torneio_idTorneio`),
+  INDEX `fk_Utilizador_has_Torneio_Torneio1_idx` (`Torneio_idTorneio` ASC) VISIBLE,
+  INDEX `fk_Utilizador_has_Torneio_Utilizador1_idx` (`Utilizador_idUtilizador` ASC) VISIBLE,
+  CONSTRAINT `fk_Utilizador_has_Torneio_Utilizador1`
+    FOREIGN KEY (`Utilizador_idUtilizador`)
+    REFERENCES `primeG`.`Utilizador` (`idUtilizador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Utilizador_has_Torneio_Torneio1`
+    FOREIGN KEY (`Torneio_idTorneio`)
+    REFERENCES `primeG`.`Torneio` (`idTorneio`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
