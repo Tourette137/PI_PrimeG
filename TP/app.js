f1f330f30e7030fabd71d96b6a77d4c98254d3b9
Bruno Dias <brunofsdias@gmail.com>
2022-11-30 00:30:05 +0000

    Login e Registo Backend
    - Login e Registo com Backend totalmente feito;
    - Encriptação implementada com salt de 10;
    - Falta verificar Autenticação / JWT;

--- TP/app.js
+++ TP/app.js
@@ -2,7 +2,6 @@ const Joi = require('joi');
 const express= require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
 
 //const port = process.env.PORT || 3000;
 
@@ -10,6 +9,9 @@ const port = 3000;
 
 app.listen(port, () => console.log('Listening on port ' + port + '!'));
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const userRouter = require("./routers/users");
 const torneioRouter = require("./routers/torneio");
--- TP/routers/users.js
+++ TP/routers/users.js
@@ -1,18 +1,66 @@
 const { application } = require('express');
 const express= require('express');
 const router = express.Router();
-const data = require('../query.js')
+const data = require('../query.js');
+const bcrypt = require('bcryptjs');
 
 
-//imprimir todos os utilizadores
-router.get("/",(req,res) => {
+//Imprimir todos os utilizadores
+router.get("/", (req, res) => {
     let sql = "Select * from utilizador;"
     data.query(sql).then(re => {
-        res.send(re);
-    });
+        res.send(JSON.stringify(re))
+    })
+})
+
+
+//Registo de um utilizador 
+router.post("/registo", (req, res) => {
+    
+    let sql = `Select * from utilizador WHERE email = "${req.body.email}";`
+
+    data.query(sql)
+        .then( async re => {
+            if(re.length == 0) {
+                hashedPass = await bcrypt.hash(req.body.password, 10)
+
+                let sql = `Insert into utilizador (Nome, email, password, dataNascimento, genero) 
+                    values ("${req.body.nome}", "${req.body.email}", "${hashedPass}", "${req.body.dataNascimento}", "${req.body.genero}")`
+                data.query(sql)
+                    .then(re => {
+                        res.send("Utilizador registado com sucesso")
+                    })
+                    .catch(e => { res.status(400).jsonp({ error: e }) })
+            } else {
+                res.status(500).jsonp("User já existe!")
+            }
+        })
+        .catch(e => { res.status(500).jsonp({ error: e }) })
+        
 })
 
 
 
-module.exports = router
 
+//Login de um Utilizador
+router.post("/login", (req, res) => {
+
+    let sql = `Select * from utilizador WHERE email = "${req.body.email}";`
+
+    data.query(sql)
+        .then( async re => {
+            if(re.length == 0) {
+                res.status(500).jsonp("User não existe!")
+            } else {
+                if (await bcrypt.compare(req.body.password, re[0].password)) {
+                    res.send("Está Logado!")
+                } else {
+                    res.status(500).jsonp("Password incorreta!")
+                }
+            }
+        })
+        .catch(e => { res.status(500).jsonp({ error: e }) })
+})
+
+
+module.exports = router
