7bfdad0531f4402fef4f0e4ef7b71893c2b2a0ac
Bruno Dias <brunofsdias@gmail.com>
2022-12-30 00:59:30 +0000

    Criação de Triggers para as Notificações e Upload da MockUp
    - Correr este código antes no inicio na criação da Base de Dados;
    - Dava jeito ter o Nome do Desporto e da Localidade e do Dia do Torneio na Notificação;

---
+++ NotificationTriggers.sql
@@ -0,0 +1,42 @@
+use primeg;
+
+DROP TRIGGER IF EXISTS torneio_trigger;
+
+DELIMITER $$
+CREATE TRIGGER torneio_trigger AFTER INSERT ON Torneio FOR EACH ROW 
+BEGIN
+	INSERT INTO Notificacao (Titulo, Torneio_idTorneio) VALUES (NEW.nomeTorneio, NEW.idTorneio);
+END$$
+DELIMITER ;
+
+
+
+DROP TRIGGER IF EXISTS notificacao_trigger;
+
+DELIMITER $$
+CREATE TRIGGER notificacao_trigger AFTER INSERT ON Notificacao FOR EACH ROW 
+BEGIN
+	INSERT INTO Utilizador_has_Notificacao (
+		SELECT DF.Utilizador_idUtilizador, NEW.idNotificacao, 0
+			FROM Notificacao as N
+			JOIN Torneio as T ON T.idTorneio = N.Torneio_idTorneio
+			JOIN DesportosFav as DF ON DF.Desporto_idDesporto = T.idDesporto
+			WHERE N.idNotificacao = NEW.idNotificacao
+		UNION
+		SELECT LF.Utilizador_idUtilizador, NEW.idNotificacao, 0
+			FROM Notificacao as N
+			JOIN Torneio as T ON T.idTorneio = N.Torneio_idTorneio
+			JOIN Espaco as E ON E.idEspaco = T.Espaco_idEspaco
+			JOIN localFav as LF ON LF.Localidade_idLocalidade = E.Localidade_idLocalidade
+			WHERE N.idNotificacao = NEW.idNotificacao
+		);
+END$$
+DELIMITER ;
+
+
+-- use primeg;
+-- INSERT INTO Notificacao (Titulo, Torneio_idTorneio) VALUES ("TEXTO", 1);
+-- INSERT INTO DesportosFav (Utilizador_idUtilizador, Desporto_idDesporto) VALUES (1, 1);
+-- INSERT INTO DesportosFav (Utilizador_idUtilizador, Desporto_idDesporto) VALUES (2, 1);
+-- INSERT INTO localFav VALUES (1, 2);
+-- INSERT INTO localFav VALUES (1, 3);
