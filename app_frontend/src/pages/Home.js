4de3b1c34e350ac6592368a822adb27ad98ba691
Luís Sobral <luisfilipesobral@gmail.com>
2023-02-01 18:03:18 +0000

    pagina inicial

--- app_frontend/src/pages/Home.js
+++ app_frontend/src/pages/Home.js
@@ -1,11 +1,34 @@
 import {NavbarDynamic} from '../components/NavbarDynamic.js';
 import {Pricing} from '../components/Pricing.js';
+import logo from '../images/logotipo.png';
 
 export function Home() {
     return(
         <>
-            <h1>Página Inicial</h1>
+            <section class="relative bg-white overflow-hidden">
 
+              <div class="py-20 md:py-52">
+                <div class="container px-4 mx-auto">
+                  <div class="flex flex-wrap xl:items-center -mx-4">
+                    <div class="w-full md:w-1/2 px-4 mb-16 md:mb-0">
+                      <span class="inline-block py-px px-2 mb-4 text-xs leading-5 text-white bg-orange-500 uppercase rounded-9xl" contenteditable="false">Sports</span>
+                      <h1 class="mb-6 text-3xl md:text-5xl lg:text-6xl leading-tight font-bold tracking-tight" contenteditable="false">A way to make your tournaments come to life
+                      </h1>
+                      <p class="mb-8 text-lg md:text-xl text-coolGray-500 font-medium">Plan your next tournment in record time with matchUp</p>
+                      <div class="flex flex-wrap">
+                        <div class="w-full md:w-auto py-1 md:py-0 md:mr-4"><a class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-orange-50 font-medium text-center bg-orange-500 hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50 border border-orange-500 rounded-md shadow-sm" href="#">Create a free tournment</a></div>
+                        <div class="w-full md:w-auto py-1 md:py-0"><a class="inline-block py-5 px-7 w-full text-base md:text-lg leading-4 text-coolGray-800 font-medium text-center bg-white hover:bg-coolGray-100 focus:ring-2 focus:ring-coolGray-200 focus:ring-opacity-50 border border-coolGray-200 rounded-md shadow-sm" href="login">Sign Up</a></div>
+                      </div>
+                    </div>
+                    <div class="w-full md:w-1/2 px-4">
+                      <div class="relative mx-auto md:mr-0 max-w-max">
+                        <img class="relative rounded-7xl" src={logo} alt="Logo"/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
 
 
             <section id="product" class="py-24 overflow-hidden">
